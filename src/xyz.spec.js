985bc929b2948ffea59c6df176e7fa19deb5b17f
Josh Justice <me@codingitwrong.com>
2021-08-09 07:21:04 -0400

    Next error

--- src/xyz.spec.js
+++ src/xyz.spec.js
@@ -1,10 +1,26 @@
 import {xyzFilename} from './xyz';
 
+function disallowUndefinedProperties(obj) {
+  const handler = {
+    get(target, property) {
+      if (property in target) {
+        return target[property];
+      }
+
+      throw new Error(`Property '${property}' is not defined`);
+    },
+  };
+
+  return new Proxy(obj, handler);
+}
+
 describe('xyz', () => {
   describe('xyzFilename', () => {
     it('works', () => {
-      const stub = undefined;
-      const subject = xyzFilename(stub);
+      const target = disallowUndefinedProperties({
+        publishOn: new Date(2021, 3, 14),
+      });
+      const subject = xyzFilename(target);
       expect(subject).toEqual('something');
     });
   });
