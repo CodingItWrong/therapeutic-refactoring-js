436926c73eb72846536f5ca48b9e79e2a1aeaf53
Josh Justice <me@codingitwrong.com>
2021-08-09 07:44:34 -0400

    Exercise removing underscores

--- src/xyz.spec.js
+++ src/xyz.spec.js
@@ -20,13 +20,15 @@ describe('xyz', () => {
       const target = disallowUndefinedProperties({
         publishOn: new Date(2021, 2, 7),
         xyzCategoryPrefix: 'abc',
-        kind: 'unicorn',
+        kind: 'magic_unicorn',
         isPersonal: false,
         id: 1337,
         title: 'magic & superglue',
       });
       const subject = xyzFilename(target);
-      expect(subject).toMatch(/07abcunicorn_1337_[0-9a-f]{8}_magicsuper\.jpg/);
+      expect(subject).toMatch(
+        /07abcmagicunicorn_1337_[0-9a-f]{8}_magicsuper\.jpg/,
+      );
     });
   });
 });
